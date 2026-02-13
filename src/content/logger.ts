8bab6586a43240d60d8b6ba74a496637be5cc72c
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-02-13 11:55:36 +0800

    chore: change debug log color

--- src/content/logger.ts
+++ src/content/logger.ts
@@ -1,5 +1,5 @@
 export default {
 	log: (...args: any[]) => console.log('[Hook Vue]', ...args),
 	error: (...args: any[]) => console.error('\x1b[31m[Hook Vue]\x1b[0m', ...args),
-	debug: (...args: any[]) => process.env.NODE_ENV === 'development' && console.debug('\x1b[34m[Hook Vue]\x1b[0m', ...args),
+	debug: (...args: any[]) => process.env.NODE_ENV === 'development' && console.debug('\x1b[33m[Hook Vue]\x1b[0m', ...args),
 }
