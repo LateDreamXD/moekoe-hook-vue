943f27aee80ebfa6bb5c21348c5c77c3705f85d4
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-02-13 13:17:55 +0800

    revert: "build: allow build for develop"
    This reverts commit 131cbcc0aced5ebc73f335b8cfd0dd4db2d0c1da.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-	define: { 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production') },
+	define: { 'process.env.NODE_ENV': JSON.stringify('production') },
 	entry: { content: 'src/content/index.ts' },
 	minify: true,
 	dts: false,
